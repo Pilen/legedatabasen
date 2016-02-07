9ace94b0a0cf233a8218c164e3fcf09e57ab9cbc
Pilen <fiskomaten@gmail.com>
2016-02-07 21:49:04 +0100

    extract now works on csv files exported from google sheets

--- extract.py
+++ extract.py
@@ -2,6 +2,8 @@
 # This python script extracts data from a simple tabs separated file, like the result from copying a google docs sheet into a text file.
 # It will put each leg in the subfolder "lege"
 
+import csv
+import sys
 import os.path
 from fileinput import input
 
@@ -9,40 +11,39 @@ def strn(s):
     return s
     return '"'+s+'"'
 
-jsonlist = []
-for line in input():
-    line = line.replace(" ", " ")
+if len(sys.argv) != 2:
+    raise Exception("Wrong number of arguments")
+with open(sys.argv[1]) as csv_file:
+    csv_file_content = csv_file.read().replace(" ", " ").split("\n")
 
-    items = [item.strip() for item in line.split("	")]
-    while len(items) < 12:
-        items.append("null")
+reader = csv.reader(csv_file_content)
 
-    filename = items[0].lower().replace(" ", "_").replace("/", "|") + ".leg"
+for row in reader:
+    while len(row) < 12:
+        row.append("null")
+
+    filename = row[0].lower().replace(" ", "_").replace("/", "|") + ".leg"
     filename = os.path.join("lege", filename)
 
-    taglist = (strn(tag.strip()) for tag in items[5].split(","))
+    taglist = (strn(tag.strip()) for tag in row[5].split(","))
     tags = ",".join(taglist)
 
-    tags = items[5]
+    tags = row[5]
 
-    result = ["name: " + strn(items[0]),
-              "teaser: " + strn(items[1]),
-              "inside: " + ("true" if items[3] == "Yes" else "false"),
-              "youtube: " + ("" if items[4] == "" else strn("youtube.com/watch?v=" + items[4])),
-              "tags: " + items[5],
-              "min_age: " + items[6],
-              "max_age: " + items[7],
-              "min_time: " + items[8],
-              "max_time: " + items[9],
-              "min_participants: " + items[10],
-              "max_participants: " + items[11],
-              "description: " + strn(items[2])
+    result = ["name: " + strn(row[0]),
+              "teaser: " + strn(row[1]),
+              "inside: " + ("true" if row[3] == "Yes" else "false"),
+              "youtube: " + ("" if row[4] == "" else strn("youtube.com/watch?v=" + row[4])),
+              "tags: " + row[5],
+              "min_age: " + row[6],
+              "max_age: " + row[7],
+              "min_time: " + row[8],
+              "max_time: " + row[9],
+              "min_participants: " + row[10],
+              "max_participants: " + row[11],
+              "description: " + strn(row[2])
     ]
-    json = "  {\n" + ",\n".join("    "+r for r in result) + "\n  }"
-    # jsonlist.append(json)
+    # json = "  {\n" + ",\n".join("    "+r for r in result) + "\n  }"
     content = "\n".join(result)
     with open(filename, "w") as file:
         file.write(content)
-    # print(content + "\n")
-
-# print("",\n".join(jsonlist))
