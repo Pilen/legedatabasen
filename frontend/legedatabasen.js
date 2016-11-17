7296ed06307d454d0111e4fae6fef39a634c1327
Piltoft <jens@piltoft.dk>
2016-11-17 14:16:57 +0100

    Update legedatabasen.js

--- frontend/legedatabasen.js
+++ frontend/legedatabasen.js
@@ -86,8 +86,8 @@ function init() {
                  '<table style="width:100%;">'+
                  '<tbody>'+
                  '<tr>'+
-                 '<td style="width:10%"><span class="fdficon" style="font-size:25pt;">&#xf405;</span></td><td style="width:15%">' + leg.participants + '</td>'+
-                 '<td style="width:10%"><span class="fdficon" style="font-size:25pt;">&#xf3ba;</span></td><td style="width:20%">' + leg.duration + '<br>min</td>'+
+                 '<td style="width:10%"><span class="fdficon" style="font-size:25pt;">&#xf405;</span></td><td style="width:10%">' + leg.participants + '</td>'+
+                 '<td style="width:10%"><span class="fdficon" style="font-size:25pt;">&#xf3ba;</span></td><td style="width:25%">' + leg.duration + '<br>min</td>'+
                  '<td style="width:10%"><span class="fdficon" style="font-size:25pt;">&#xf41e;</span></td><td style="width:10%">' + leg.age + '+</td>'+
                  '<td style="width:10%"><span class="fdficon" style="font-size:25pt;">&#xf360;</span></td><td style="width:15%">' + leg.area + '</td>'+
                  '</tr>'+
